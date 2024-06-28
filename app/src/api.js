e5ebe82c6313b3ae3796d40322861b5f967421da
jahmarimaxwell <jahmarimaxwell@gmail.com>
2024-06-27 21:53:13 -0400

    changed on main by accident, created button and displayed extra info on click

--- app/src/api.js
+++ app/src/api.js
@@ -46,8 +46,59 @@ const renderUserInfo = (newUserSpace, users) => {
     });
     div.appendChild(img); //appending img el to div
     div.appendChild(ul); //appending entire unordered list to div
+
+    const moreInfoButton = document.createElement("button"); //create button for more info
+    const moreInfoContainer = document.createElement("div"); //create container to hold more info from user
+    moreInfoContainer.setAttribute("id", "more-info-container");
+
+    const appDiv = document.querySelector('#app'); //  selects app div so that the extra info is seperated
+    
+    appDiv.append(moreInfoContainer);
+
+    moreInfoContainer.append(moreInfoButton); // appends more info button to appDiv
+    moreInfoButton.style.margin = "20px" // styled through javascript
+    moreInfoButton.setAttribute("id", "more-info-button"); //setting attribute  of moreInfoButton
+    moreInfoButton.textContent = `Press for more content` // setting text content of button
+
+
+    moreInfoButton.addEventListener('click', () => { // click event on more info button
+
+        moreInfoButton.remove(); // removes button upon click
+        console.log('Button has been clicked!'); // logging to the console as test
+        const properties2 = ['email', 'phone']; //storing user api obj key names in an arr
+
+        properties2.forEach(prop => { //iterating through properties arr to access each random user profiles properties by the api obj key name
+            const li = document.createElement("li"); //creating a list item for other properties
+            li.setAttribute("id", "more-properties") //setting attribute for both elements to be selected properly
+            li.textContent = user[prop] || user[prop]; //setting text content based on user data availability from the api obj key/value pair
+            moreInfoContainer.appendChild(li); // appends each of li to appDiv
+        })
+        // Here you can define what happens when the image is clicked
+        // For example, you can generate more user info or perform any other action
+
+        const div = document.createElement("div"); //creating a div for locaction, username and password
+        div.setAttribute("id", "address-username-container"); //sets attribute of div
+
+        const p = document.createElement("p"); //creating a p for different text content
+        const p2 = document.createElement("p"); //creating a p for different text content
+        const p3 = document.createElement("p"); // creating a p for every section of text 
+
+        p.textContent = `My address is ${user.location.street['number']} ${user.location.street['name']}`
+        p2.textContent = `If your need my login for your database needs:`
+        p3.textContent = ` Username: ${user.login['username']} Password: ${user.login['password']}`
+
+        div.append(p,p2,p3);
+        appDiv.appendChild(div); 
+
+        console.log('User information:', users); // Example: Log user information to console
+        // You can extend this to display more user information or perform any desired action
+      });
     fragment.appendChild(div); //appending entire div to doc fragment
+
+
   });
+
+  
   newUserSpace.append(fragment); //appending fragment to newUserSpace in one operation
 };
 
